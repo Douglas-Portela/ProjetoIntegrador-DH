392a3f8745531bf82d4feda0f99809b246581314
Douglas Portela <dg.portela.santos@gmail.com>
2022-07-27 16:01:44 -0300

    Atualização das paginas de Cadastro, login, stye e routers

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('cadastro');
-});
+app.get('/', (req, res) => {res.send('')});
+app.get("/cadastro/cadastro.html", (req, res) => {res.send ("cadastro")})
+app.get("/esqueciMinhaSenha/esqueciSenha.html", (req, res) => {res.send ("Esqueci Senha")})
+app.get("/login/login.html", (req, res) => {res.send ("login")})
+app.get("/paginaDePerfil/perfil.html", (req, res) => {res.send ("perfil")})
 
-app.listen(3000, 0) ();
+app.listen(port, () => {
+  console.log()
+})
